a43a09478a1f346ec1d546bf25396fefa2da224b
MJ <eoqkrskfk94@naver.com>
2021-06-17 00:15:22 +0900

    Initial commit

---
+++ app/src/main/java/com/mj/aop_part4_chapter02/PlayerFragment.kt
@@ -0,0 +1,24 @@
+package com.mj.aop_part4_chapter02
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class PlayerFragment : Fragment(R.layout.fragment_player) {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    companion object {
+        fun newInstance(): PlayerFragment {
+            return PlayerFragment()
+        }
+    }
+
+
+}
